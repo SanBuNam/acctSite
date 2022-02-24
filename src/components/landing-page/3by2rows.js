31559d5d0061af1e57c0d1aac4bdfde260348395
David Lee <stoicSanbunam@gmail.com>
2022-02-23 21:25:06 -0800

    update

--- src/components/landing-page/3by2rows.js
+++ src/components/landing-page/3by2rows.js
@@ -1,35 +1,39 @@
 import React from 'react'
 import styled from 'styled-components'
 import styles from "./3by2rows.module.scss"
-import Accountingwhite from "../../../content/images/Accounting-white.svg"
+import Accounting from "../../../content/images/Accounting-white.svg"
+import TailoredAdvisory from "../../../content/images/TailoredAdvisory-white.svg"
+import Audit from "../../../content/images/Audit-white.svg"
+import Assets from "../../../content/images/Assets-white.svg"
+import Taxes from "../../../content/images/Taxes-white.svg"
 
 const ThreeByTwoContainer = () => {
 
     return (
         <div className={styles.container}>
             <div>
-			    <img src={Accountingwhite} width="64" height="82" alt="Accounting Icon - Calculator"/>
+			    <img src={Accounting} width="64" height="82" alt="Accounting Icon - Calculator"/>
                 <h3>MANAGE ACCOUNTING</h3>
             </div>
             <div>
-			    <img src={Accountingwhite} width="64" height="82" alt="Accounting Icon - Calculator"/>
-                <h3>MANAGE ACCOUNTING</h3>
+                <img src={Accounting} width="64" height="82" alt="Accounting Icon - Calculator"/>
+                <h3>WE PROVIDE SOLUTIONS</h3>
             </div>
             <div>
-			    <img src={Accountingwhite} width="64" height="82" alt="Accounting Icon - Calculator"/>
-                <h3>MANAGE ACCOUNTING</h3>
+			    <img src={TailoredAdvisory} width="64" height="82" alt="Accounting Icon - Calculator"/>
+                <h3>TAILORED ADVISORY</h3>
             </div>
             <div>
-			    <img src={Accountingwhite} width="64" height="82" alt="Accounting Icon - Calculator"/>
-                <h3>MANAGE ACCOUNTING</h3>
+			    <img src={Audit} width="64" height="82" alt="Accounting Icon - Calculator"/>
+                <h3>SIMPLIFY YOUR AUDIT</h3>
             </div>
             <div>
-			    <img src={Accountingwhite} width="64" height="82" alt="Accounting Icon - Calculator"/>
-                <h3>MANAGE ACCOUNTING</h3>
+			    <img src={Assets} width="64" height="82" alt="Accounting Icon - Calculator"/>
+                <h3>PROTECT YOUR ASSETS</h3>
             </div>
             <div>
-			    <img src={Accountingwhite} width="64" height="82" alt="Accounting Icon - Calculator"/>
-                <h3>MANAGE ACCOUNTING</h3>
+			    <img src={Taxes} width="64" height="82" alt="Accounting Icon - Calculator"/>
+                <h3>MINIMIZE YOUR TAXES</h3>
             </div>
         </div>
     )
